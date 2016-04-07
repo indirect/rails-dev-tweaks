c62f332a6b0b66e6ab9b7e07114073211f72f897
Arturo Pie <arturop@nulogy.com>
2016-04-07 16:43:10 -0400

    reload_dependencies? method is not defined in Rails.application in Rails 4.1. Inline its implementation

--- lib/rails_dev_tweaks/granular_autoload/middleware.rb
+++ lib/rails_dev_tweaks/granular_autoload/middleware.rb
@@ -42,8 +42,6 @@ class RailsDevTweaks::GranularAutoload::Middleware
   def reload_dependencies?
     application = Rails.application
 
-    # Rails 3.2 defines reload_dependencies? and it only reloads if reload_dependencies? returns true.
-      (!application.class.method_defined?(:reload_dependencies?) ||
-        application.send(:reload_dependencies?))
+    application.config.reload_classes_only_on_change != true || application.reloaders.map(&:updated?).any?
   end
 end
